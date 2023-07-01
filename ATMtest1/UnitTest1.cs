4dba93cf8dea4ba016e1186f92474b0342047277
Tasneem <117952461+Tasneem-Alabed@users.noreply.github.com>
2023-07-01 20:19:10 +0300

    Update UnitTest1.cs

--- ATMtest1/UnitTest1.cs
+++ ATMtest1/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace ATMtest1
         [Fact]
         public void TestWithdraw()
         {
+            ATM.Program.Balance = 10;
             decimal x = 9;
             decimal result = ATM.Program.Withdraw(x);
             Assert.Equal(result, 1);
@@ -14,6 +15,7 @@ namespace ATMtest1
         [Fact]
         public void TestWithdraw2()
         {
+            ATM.Program.Balance = 10;
             decimal x = 0;
             decimal result = ATM.Program.Withdraw(x);
             Assert.Equal(result, 10);
@@ -21,6 +23,7 @@ namespace ATMtest1
         [Fact]
         public void TestWithdraw3()
         {
+            ATM.Program.Balance = 10;
             decimal x = 5;
             decimal result = ATM.Program.Withdraw(x);
             Assert.Equal(result, 5);
@@ -29,6 +32,7 @@ namespace ATMtest1
         [Fact]
         public void Deposit()
         {
+            ATM.Program.Balance = 10;
             decimal x = 10;
             var result = ATM.Program.Deposit(x);
             Assert.Equal(result, 20);
@@ -37,6 +41,7 @@ namespace ATMtest1
         [Fact]
         public void Deposit2()
         {
+            ATM.Program.Balance = 10;
             decimal x = 5;
             var result = ATM.Program.Deposit(x);
             Assert.Equal(result, 15);
@@ -45,12 +50,14 @@ namespace ATMtest1
         [Fact]
         public void view()
         {
+            ATM.Program.Balance = 10;
             var result = ATM.Program.ViewBalance;
             Assert.Equal(result, result);
         }
         [Fact]
         public void Deposit3()
         {
+            ATM.Program.Balance = 10;
             decimal x = 3;
             var result = ATM.Program.Deposit(x);
             Assert.Equal(result, 13);
